4ceea29913d1d0f638b836bb0a982d812cf6c719
rakamike <muaaiken0726@gmail.com>
2020-02-22 23:00:03 +0900

    ahoge 31st entry verion.

--- typescript/parts.ts
+++ typescript/parts.ts
@@ -149,6 +149,30 @@ class Parts {
         });
         return new C_BoundText(str, x, y, size, color, maxTime);
     }
+    TextPoint(point, x, y, size, lifeTime) {
+        //テキストバウンドクラス
+        var C_PointText = Class.create(Group, {
+
+            initialize: function initialize(point, x, y, size, lifeTime) {		//コンストラクタ(表示数字,x,y)
+                Group.call(this);
+                this.lifeTime = lifeTime;
+                this.Label = new Label(point);		//enchant.jsでは文字表示はLabelオブジェクトを使う。
+                this.Label.font = size + "px Meiryo";			//メイリオで表示
+                this.Label.color = "white";					//赤で表示
+                this.x = x;							//引数に現在座標をとる（指定位置に表示させたいので）
+                this.y = y;							//同上
+                this.WhiteLabel = new Label(point);
+                this.WhiteLabel.x = -2;
+                this.WhiteLabel.y = -1;
+                this.WhiteLabel.font = (size + 2) + "px Meiryo";	//メイリオで表示
+                this.WhiteLabel.color = "red";					//白で表示
+
+                this.addChild(this.WhiteLabel);
+                this.addChild(this.Label);
+            }
+        });
+        return new C_PointText(point, x, y, size, lifeTime);
+    }
     SpriteBound(image, frame, x, y, vx) {
         //テキストバウンドクラス
         var C_SpriteBound = Class.create(Sprite, {
@@ -182,6 +206,145 @@ class Parts {
             }
         });
         return new C_SpriteBound(image, frame, x, y, vx);
+    }
+    SpriteAccel(image, frame, x, y, ax, ay) {
+        //加速度ありスプライト
+        var C_SpriteAccel = Class.create(Sprite, {
+            ay: 0,		//Y軸加速度
+            ax: 0,		//X軸加速度
+            speed: 1,
+            initialize: function initialize(image, frame, x, y, ax, ay) {		//コンストラクタ(表示数字,x,y)
+                Sprite.call(this, 100, 100);
+                this.scale(1, 1);
+                this.image = image;
+                this.frame = frame;
+                this.x = x - 50;
+                this.y = y - 50;
+                this.ax = ax;
+                this.ay = ay;
+                this.speed = 0;
+            },
+            onenterframe: function onenterframe() {		//毎フレーム毎に呼び出す処理
+                this.y += this.ay * this.speed;
+                this.x += this.ax * this.speed;
+                if (this.ax != 0) {
+                    this.rotation = this.rotation + 5;
+                }
+                if (this.x + (this.width / 2) + (this.width / 2 * this.scaleX) > 400) {
+                    //右端にぶつかった判定
+                    this.ax = -(Math.abs(this.ax));
+                    this.x = 400 - (this.width / 2) - (this.width / 2 * this.scaleX);
+                } else if (this.x + (this.width / 2) - (this.width / 2 * this.scaleX) < 0) {
+                    //左
+                    this.ax = Math.abs(this.ax);
+                    this.x = 0 - (this.width / 2) + (this.width / 2 * this.scaleX);
+                }
+                //if (this.y > 500) {
+                if (this.y + (this.height / 2) + (this.height / 2 * this.scaleY) > 600) {
+                    //下
+                    this.ay = -(Math.abs(this.ay));
+                    this.y = 600 - (this.height / 2) - (this.height / 2 * this.scaleY);
+                } else if (this.y + (this.height / 2) - (this.height / 2 * this.scaleY) < 0) {
+                    //上
+                    this.ay = Math.abs(this.ay);
+                    this.y = 0 - (this.height / 2) + (this.height / 2 * this.scaleY);
+                }
+                //if (this.age % 20 == 0) this.scale(1.05, 1.05);
+                let scale = 0;
+                scale = 0.3 + this.y * (0.5 / 500);
+                this.scaleX = scale * scale;
+                this.scaleY = scale * scale;
+                this.speed = scale * scale;
+            }
+        });
+
+        return new C_SpriteAccel(image, frame, x, y, ax, ay);
+    }
+    SpriteTarget(image, frame, x, y, ax, ay) {
+        //加速度ありスプライト
+        var C_SpriteTarget = Class.create(Sprite, {
+            ay: 0,		//Y軸加速度
+            ax: 0,		//X軸加速度
+            speed: 1,
+            initialize: function initialize(image, frame, x, y, ax, ay) {		//コンストラクタ(表示数字,x,y)
+                Sprite.call(this, 100, 100);
+                // this.scale(2, 2);
+                this.image = image;
+                this.frame = frame;
+                this.x = x - 50;
+                this.y = y - 50;
+                this.ax = ax;
+                this.ay = ay;
+                this.speed = 1;
+            },
+            onenterframe: function onenterframe() {		//毎フレーム毎に呼び出す処理
+                this.y += this.ay * this.speed;
+                this.x += this.ax * this.speed;
+                if (this.x + (this.width / 2) + (this.width / 2 * this.scaleX) > 400) {
+                    //右端にぶつかった判定
+                    this.ax = -(Math.abs(this.ax));
+                    this.x = 400 - (this.width / 2) - (this.width / 2 * this.scaleX);
+                } else if (this.x + (this.width / 2) - (this.width / 2 * this.scaleX) < 0) {
+                    //左
+                    this.ax = Math.abs(this.ax);
+                    this.x = 0 - (this.width / 2) + (this.width / 2 * this.scaleX);
+                }
+                //if (this.y > 500) {
+                if (this.y + (this.height / 2) + (this.height / 2 * this.scaleY) > 500) {
+                    //下
+                    this.ay = -(Math.abs(this.ay));
+                    this.y = 500 - (this.height / 2) - (this.height / 2 * this.scaleY);
+                } else if (this.y + (this.height / 2) - (this.height / 2 * this.scaleY) < 50) {
+                    //上
+                    this.ay = Math.abs(this.ay);
+                    this.y = 50 - (this.height / 2) + (this.height / 2 * this.scaleY);
+                }
+                //if (this.age % 20 == 0) this.scale(1.05, 1.05);
+                let scale = 0;
+                scale = 0.3 + this.y * (0.5 / 500);
+                this.scaleX = scale * scale * 2;
+                this.scaleY = scale * scale * 2;
+                this.speed = scale * scale;
+            }
+        });
+
+        return new C_SpriteTarget(image, frame, x, y, ax, ay);
+    }
+
+    SpritePoint(image, frame, x, y, width, height) {
+        //ポイントスプライト
+        var C_SpriteAccel = Class.create(Sprite, {
+            initialize: function initialize(image, frame, x, y, width, height) {
+                Sprite.call(this, width, height);
+                this.image = image;
+                this.frame = frame;
+            }
+        });
+
+        return new C_SpriteAccel(image, frame, x, y, width, height);
+    }
+    SpriteLifeTime(image, frame, x, y, lTime) {
+        //寿命ありスプライト
+        var C_SpriteAccel = Class.create(Sprite, {
+            initialize: function initialize(image, frame, x, y) {		//コンストラクタ(表示数字,x,y)
+                Sprite.call(this, 100, 100);
+                this.image = image;
+                this.frame = frame;
+                this.x = x - 50;
+                this.y = y - 50;
+                this.scale(0.3, 0.3);
+                this.lifeTIme = lTime;
+            },
+            onenterframe: function onenterframe() {		//毎フレーム毎に呼び出す処理
+                this.lifeTIme--;
+                this.scale(this.lifeTIme / lTime, this.lifeTIme / lTime);
+                if (this.lifeTIme <= 0) {				//消滅用カウントが０になったら
+                    this.parentNode.removeChild(this);	//自分をけす。
+                }
+            }
+        });
+
+        return new C_SpriteAccel(image, frame, x, y);
     }
     upText(str, x, y, size, color) {
         //テキストバウンドクラス
--- typescript/script.ts
+++ typescript/script.ts
@@ -3,19 +3,28 @@ import Loader from "./loader";
 import parts from "./parts";
 
 enchant();
-
-
 window.onload = function () {
     const game = new Game(400, 600);
-    game.fps = 30;
-    const pImgs = Loader.ImageLoader(game);
-    const pMscs = Loader.SoundLoader(game);
+    game.fps = 60;
+    // const pImgs = Loader.ImageLoader(game);
+    // const pMscs = Loader.SoundLoader(game);
 
     let score = 0;
-
+    let time = 0;
+    game.preload('./image/newspaper.png');
+    game.preload('./image/ball_pink.png');
+    // game.preload('./image/background.png');
+    game.preload('./image/back.jpg');
+    game.preload('./image/house.png');
+    game.preload('./sound/hit.mp3');
+    game.preload('./sound/clear.mp3');
+    let firstClick = 0;
+    let stage = 1; //初期ステージ
     game.onload = function () {
         const setScene = function (sceneNum: number) {
             const scene = new Scene();
+            const bg = parts.SpritePoint(game.assets['./image/back.jpg'], 0, 0, 0, 400, 600);
+            scene.addChild(bg);
             scene.backgroundColor = "black";
             try {
                 game.popScene();
@@ -25,41 +34,178 @@ window.onload = function () {
             game.pushScene(scene);
             switch (sceneNum) {
                 case 0: {
+                    const points = [];
+                    let wk;
+                    if (stage == 1) {
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 0, 51, 400, 0, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 1, 200, 400, 0, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 2, 400, 400, 0, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                    } else if (stage == 2) {
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 0, 20, 200, 0, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 1, 200, 200, 0, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 2, 380, 200, 0, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                    } else if (stage == 3) {
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 0, 50, 200, 0, 1);
+                        points.push(wk);
+                        scene.addChild(wk);
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 1, 200, 400, -1, 1);
+                        points.push(wk);
+                        scene.addChild(wk);
+                        wk = parts.SpriteTarget(game.assets['./image/house.png'], 2, 350, 300, 1, 0);
+                        points.push(wk);
+                        scene.addChild(wk);
+                    } else if (stage == 4) {
+                        for (let idx = 0; idx < 6; idx++) {
+                            wk = parts.SpriteTarget(game.assets['./image/house.png'], rand(7), rand(400), 100 + rand(300), Math.random() * 3, Math.random() * 3);
+                            points.push(wk);
+                            scene.addChild(wk);
+                        }
+                    } else if (stage == 5) {
+                        for (let idx = 0; idx < 20; idx++) {
+                            wk = parts.SpriteTarget(game.assets['./image/house.png'], rand(7), rand(400), 100 + rand(300), Math.random() * 10, Math.random() * 10);
+                            points.push(wk);
+                            scene.addChild(wk);
+                        }
+                    } else if (stage == 6) {
+                        for (let idx = 0; idx < 20; idx++) {
+                            wk = parts.SpriteTarget(game.assets['./image/house.png'], rand(7), rand(400), 100 + rand(300), Math.random() * 10, Math.random() * 10);
+                            points.push(wk);
+                            scene.addChild(wk);
+                        }
+                    }
+
+
                     const label = parts.Label("");
                     scene.addChild(label);
                     const showLabel = function () {
-                        label.text = "point:" + score;
+                        label.text = "stage:" + stage;
                     }
                     showLabel();
-                    const back = new Sprite(100, 100);
-                    back.image = game.assets[pImgs[1]];
-                    back.moveTo(100, 100);
-                    back.time = 0;
-                    scene.addChild(back);
+
+                    const labelTime = parts.Label("");
+                    labelTime.x = 100;
+                    scene.addChild(labelTime);
+                    const showLabelTime = function () {
+                        labelTime.text = "time:" + time;
+                    }
+                    showLabelTime();
+
+                    const ball = parts.SpriteAccel(game.assets['./image/newspaper.png'], 0, 200, 550, 0, 0);
+                    scene.addChild(ball);
+                    // const back = new Sprite(20, 20);
+                    // back.image = game.assets[pImgs[1]];
+                    // back.moveTo(100, 100);
+                    // back.time = 0;
+                    // scene.addChild(back);
                     // 毎フレームで実行するイベントハンドラ
-                    back.onenterframe = function () {
-                        this.time++;
-                        this.x += Math.sin(this.time / 30) * 2;
-                        this.y += Math.cos(this.time / 30) * 2;
+                    scene.onenterframe = function () {
+                        // 時間で終了
+                        if (this.age > game.fps * 60) setScene(1);
+                        if (this.age % game.fps == 0) {
+                            time++;
+                            showLabelTime();
+                        }
+                        if (points.length == 0) {
+                            if (stage == 5) {
+                                //クリア
+                                let sound = game.assets['./sound/clear.mp3'].clone(); //同じ音を重ねるためCloneが必要
+                                sound.play();
+                                stage = 1;
+                                setScene(3);
+                            } else {
+                                //インターバル
+                                stage += 1;
+                                setScene(4);
+                            }
+                        }
+                        points.forEach(function (point, i) {
+                            //console.log(point.x);
+                            if (ball.within(point, (point.width * point.scaleX + ball.width * ball.scaleX) / 2 * 0.9)) {
+                                scene.removeChild(point);
+                                points.splice(i, 1);
+                                let sound = game.assets['./sound/hit.mp3'].clone(); //同じ音を重ねるためCloneが必要
+                                sound.play();
+                                return;
+                            }
+                            if (stage == 6) {
+
+                            }
+                        });
                     }
                     // クリック時に実行するイベントハンドラ
-                    back.ontouchend = function () {
-                        game.assets[pMscs[1]].clone().play(); //音を鳴らす
-                        score++; // グローバル変数のスコアを１増やす
-                        showLabel(); // スコアを反映
-                        // MEMO: 作っちゃうおじさん謹製のテキストバウンドモジュールを使用する例
-                        const label = parts.TextBound("+1", this.x, this.y, 32, "orange", 60);
-                        scene.addChild(label);
-                        if (score >= 10) {
-                            score = 0;
-                            setScene(1);
+                    // back.ontouchend = function () {
+                    //     console.log("touchend");
+                    //     game.assets[pMscs[1]].clone().play(); //音を鳴らす
+                    //     score++; // グローバル変数のスコアを１増やす
+                    //     showLabel(); // スコアを反映
+                    //     // MEMO: 作っちゃうおじさん謹製のテキストバウンドモジュールを使用する例
+                    //     const label = parts.TextBound("+2", this.x, this.y, 32, "orange", 60);
+                    //     scene.addChild(label);
+                    //     if (score >= 3) {
+                    //         score = 0;
+                    //         setScene(1);
+                    //     }
+                    // }
+                    scene.ontouchstart = function (e) {
+                        const puller = parts.SpriteLifeTime(game.assets['./image/ball_pink.png'], 0, e.x, e.y, 20);
+                        scene.addChild(puller);
+                        //吸引
+                        if (firstClick == 0) {
+                            ball.ax += (puller.x - ball.x) / 10;
+                            ball.ay += (puller.y - ball.y) / 10;
+                            firstClick += 1;
+                        } else {
+                            ball.ax += (puller.x - ball.x) / 30;
+                            ball.ay += (puller.y - ball.y) / 30;
                         }
                     }
                     break;
                 }
                 case 1: {
-                    const btn = parts.Button("もう一度遊ぶ");
+                    const btn = parts.Button("もういちど");
+                    btn.moveTo(150, 200);
+                    scene.addChild(btn);
+                    btn.ontouchend = function () {
+                        time = 0;
+                        setScene(0);
+                    }
+                    break;
+                }
+                case 2: {
+                    const btn = parts.Button("すたーと");
+                    btn.moveTo(150, 200);
+                    scene.addChild(btn);
+                    btn.ontouchend = function () {
+                        setScene(0);
+                    }
+                    break;
+                }
+                case 3: {
+                    const btn = parts.Button("くりあタイム：" + time + "秒");
                     btn.moveTo(100, 200);
+                    scene.addChild(btn);
+                    btn.ontouchend = function () {
+                        stage = 1;
+                        time = 0;
+                        setScene(0);
+                    }
+                    break;
+                }
+                case 4: {
+                    const btn = parts.Button("すてーじ" + stage);
+                    btn.moveTo(150, 200);
                     scene.addChild(btn);
                     btn.ontouchend = function () {
                         setScene(0);
@@ -68,7 +214,10 @@ window.onload = function () {
                 }
             }
         }
-        setScene(0);
+        setScene(2);
     }
     game.start();
+}
+function rand(n) {
+    return Math.floor(Math.random() * (n + 1));
 }
